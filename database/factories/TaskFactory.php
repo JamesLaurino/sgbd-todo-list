91d4d84dce915336f12ea928a50cbfc108ffcd1d
jamesLaurino <petersthomas90@gmail.com>
2025-04-02 14:46:06 +0200

    set up db

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+
+class TaskFactory extends Factory
+{
+
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'completed' => fake()->unique()->safeEmail(),
+        ];
+    }
+}
