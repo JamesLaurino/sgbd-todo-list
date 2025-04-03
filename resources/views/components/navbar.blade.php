390a1bc9425b96e67c841595b13ea2ecdb6024a1
jamesLaurino <petersthomas90@gmail.com>
2025-04-03 13:19:28 +0200

    amélioration de la navigation : style the dashbord et ajout du @guest

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
     <div class="mr-auto">
-        <a class="navbar-brand ml-3 text-white" href="#"></a>
+        <a class="navbar-brand ml-3 text-white" href="#">
+            <span class="fas fa-book mt-2" style="font-size: 2rem;"></span>
+        </a>
     </div>
 
     <div class="d-flex justify-content-center w-100 ml-5 text-white">
         <span class="h3 mr-3">Ma todo liste</span>
-        <span class="fas fa-book mt-2" style="font-size: 1.5rem;"></span>
     </div>
 
     @auth
@@ -13,4 +14,10 @@
             <a class="nav-link text-white" href="/dashboard">Dashbord</a>
         </div>
     @endauth
+
+    @guest
+        <div class="my-2 my-lg-0 mr-5">
+            <a class="nav-link text-white" href="/login">connexion</a>
+        </div>
+    @endguest
 </nav>
