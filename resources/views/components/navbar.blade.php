cf665d6771e258af5e0357460cc0436425d10b6a
jamesLaurino <petersthomas90@gmail.com>
2025-04-03 13:21:32 +0200

    amélioration de la navigation : style the dashbord

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3">
     <div class="mr-auto">
-        <a class="navbar-brand ml-3 text-white" href="#">
-            <span class="fas fa-book mt-2" style="font-size: 2rem;"></span>
+        <a class="navbar-brand ml-3 text-white" href="">
+            <span class="fas fa-book" style="font-size: 1.5rem;"></span>
         </a>
     </div>
 
     <div class="d-flex justify-content-center w-100 ml-5 text-white">
-        <span class="h3 mr-3">Ma todo liste</span>
+        <span class="h3 mr-3">Ma Todo Liste</span>
     </div>
 
     @auth
